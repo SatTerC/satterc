6b61de3a540478e2368491bc51b7e45293c24d8d
jmarshrossney <17361029+jmarshrossney@users.noreply.github.com>
2026-03-28 09:55:48 +0000

    split cli into separate files

---
+++ src/satterc/cli/graph.py
@@ -0,0 +1,107 @@
+import subprocess
+import tomllib
+from pathlib import Path
+from typing import Annotated
+
+import typer
+from hamilton import graph_types
+import xarray as xr
+
+from ..config import load_config
+from ..driver import build_driver
+
+app = typer.Typer(help="Visualise a pipeline defined in a configuration file.")
+
+
+def custom_style(
+    *, node: graph_types.HamiltonNode, node_class: str
+) -> tuple[dict, str | None, str | None]:
+    """Custom style function for the visualization."""
+    if node.tags.get("module") == "satterc.inputs.static":
+        style = ({"fillcolor": "aquamarine"}, node_class, "static inputs")
+
+    elif node.type is xr.DataArray and "_daily" in node.name:
+        style = ({"fillcolor": "orange"}, node_class, "Daily")
+
+    elif node.type is xr.DataArray and "_weekly" in node.name:
+        style = ({"fillcolor": "yellow"}, node_class, "Weekly")
+
+    elif node.type is xr.DataArray and "_monthly" in node.name:
+        style = ({"fillcolor": "brown"}, node_class, "Monthly")
+
+    else:
+        style = ({}, node_class, None)
+
+    return style
+
+
+@app.command()
+def graph(
+    config_file: Annotated[
+        Path, typer.Argument(exists=True, file_okay=True, dir_okay=False, readable=True)
+    ],
+    output: Annotated[
+        str, typer.Option("-o", "--output", help="Name of output file")
+    ] = "pipeline",
+    allow_overrides: Annotated[
+        bool,
+        typer.Option(
+            "--allow-overrides",
+            help="Allow later modules to override earlier ones.",
+        ),
+    ] = False,
+    png: Annotated[bool, typer.Option(help="Convert to PNG format")] = False,
+    pdf: Annotated[bool, typer.Option(help="Convert to PDF format")] = False,
+) -> None:
+    """Visualise a pipeline defined in a configuration file.
+
+    Attention
+    ---------
+    This requires graphviz to be installed.
+    """
+    with config_file.open("rb") as file:
+        config = tomllib.load(file)
+
+    parsed = load_config(config_file)
+    graphviz_kwargs = config.get("graphviz", None)
+
+    dr = build_driver(
+        modules=parsed["modules"],
+        config=parsed["driver_config"],
+        allow_module_overrides=allow_overrides,
+    )
+
+    output_path = Path(output).with_suffix(".dot")
+
+    dr.display_all_functions(
+        output_file_path=str(output_path),
+        graphviz_kwargs=graphviz_kwargs,
+        custom_style_function=custom_style,
+    )
+    # dr.display_upstream_of(
+    #    "soil_organic_carbon_monthly",
+    #    output_file_path=str(output_path),
+    #    graphviz_kwargs=graphviz_kwargs,
+    # )
+
+    # TODO: is there a better way than this?
+    if png:
+        subprocess.run(
+            [
+                "dot",
+                "-Tpng",
+                str(output_path),
+                "-o",
+                str(output_path.with_suffix(".png")),
+            ]
+        )
+    if pdf:
+        subprocess.run(
+            [
+                "dot",
+                "-Tpdf",
+                str(output_path),
+                "-o",
+                str(output_path.with_suffix(".pdf")),
+            ]
+        )
