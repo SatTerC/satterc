32fc37a26c7dd8e86b8e5296fce41019034d0f96
jmarshrossney <17361029+jmarshrossney@users.noreply.github.com>
2026-04-10 11:13:37 +0100

    fix lint errors using __all__

--- src/satterc/setup_utils/__init__.py
+++ src/satterc/setup_utils/__init__.py
@@ -8,6 +8,15 @@ from .config_gen import (
     get_model_params,
 )
 
+__all__ = [
+    "generate_config",
+    "analyze_model_module",
+    "infer_required_data",
+    "get_builtin_models",
+    "get_model_params",
+    "BuiltinModels",
+]
+
 
 class BuiltinModels(StrEnum):
     SPLASH = "splash"
